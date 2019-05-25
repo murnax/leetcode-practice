3758b6fc3db6006175905ab8daf0d5734f037a78
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-05-25 20:40:47 +0700

    add comment

--- problems/238-productExceptSelf.js
+++ problems/238-productExceptSelf.js
@@ -1,7 +1,18 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * 1) Understanding the question
+ * Write function that take an array of non-negative integer 
+ * and return the result as an array where each index contains the product
+ * of every elements except itself.
+ * 
+ * 2) Examples
+ * nums: [1, 2, 3, 4] should returns [24 <- (2*3*4), 12 <- (1*3*4), 8 <- (1*2*4), 6 <- (1*2*3)]
+ * nums: [3, 5, 7, 9] should returns [315 <- (5*7*9), 189 <- (3*7*9), 135 <- (3*5*9), 105 <- 3*5*7)]
+ * 
+ * 
  */
+
+// https://stackoverflow.com/questions/2680548/given-an-array-of-numbers-return-array-of-products-of-all-other-numbers-no-div?page=1&tab=oldest#tab-top
+// https://leetcode.com/problems/product-of-array-except-self/discuss/65622/Simple-Java-solution-in-O(n)-without-extra-space
 var productExceptSelf = function (nums) {
     let n = nums.length;
     let pLeft = 1;
