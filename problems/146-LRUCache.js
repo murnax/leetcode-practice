d0908035642827988e097aac066de505acc77a7f
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-06-09 16:04:03 +0700

    146 lru cache

---
+++ problems/146-LRUCache.js
@@ -0,0 +1,110 @@
+class Node {
+    constructor(key, value, next = null, prev = null) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+    this.size = 0;
+    this.limit = capacity;
+    this.head = this.tail = null;
+    this.cache = {};
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+    return this.getNode(key) ? this.getNode(key).value : -1;
+};
+
+LRUCache.prototype.getNode = function (key) {
+    const node = this.cache[key];
+    if (!node) return -1;
+    const { value } = node;
+    this.remove(key);
+    this.put(key, value);
+    return node;
+}
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+
+    if (key in this.cache) {
+        this.remove(key);
+        // this.cache[key].value = value;
+    }
+
+    // remove least used data if cache already full
+    this.ensureLimit();
+
+    const node = new Node(key, value);
+    if (!this.head) {
+        this.head = this.tail = node;
+    } else {
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+    this.size++;
+    this.cache[key] = node;
+
+};
+
+LRUCache.prototype.ensureLimit = function () {
+    if (this.size === this.limit) {
+        // remove tail (least used)
+        this.remove(this.tail.key);
+    }
+}
+
+LRUCache.prototype.remove = function (key) {
+    const node = this.cache[key];
+    if (node.prev) {
+        node.prev.next = node.next;
+    } else {
+        this.head = node.next;
+    }
+
+    if (node.next) {
+        node.next.prev = node.prev;
+    } else {
+        this.tail = node.prev;
+    }
+    delete this.cache[key];
+    this.size--;
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+var cache = new LRUCache(2 /* capacity */);
+
+console.log(cache.get(2));
+cache.put(2, 6);
+console.log(cache.get(1));
+cache.put(1, 5);
+cache.put(1, 2);
+console.log(cache);
+// console.log(cache.get(1));       // returns 1
+// cache.put(3, 3);    // evicts key 2
+// console.log(cache.get(1));       // returns -1 (not found)
+// cache.put(4, 4);    // evicts key 1
+// console.log(cache.get(1));       // returns -1 (not found)
+// console.log(cache.get(3));       // returns 3
+// console.log(cache.get(4));       // returns 4
