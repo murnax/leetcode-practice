e2230c80fdc46457a10955381a9ba7159aa6f7e3
Suratin Pattanawongthai <suratin.elec@gmail.com>
2019-12-04 21:34:25 +0700

    binary tree preorder and inorder

---
+++ problems/94-binary-tree-inorder-traversal.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if (!root) return [];
+    return [root.val, ...inorderTraversal(root.left), ...inorderTraversal(root.right)]
+};
+module.exports = inorderTraversal;
