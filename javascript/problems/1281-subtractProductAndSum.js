c9764e026bff299c370d9fa965e13224159f36d1
Suratin Pattanawongthai <suratin.elec@gmail.com>
2019-12-11 22:57:52 +0700

    updat

--- javascript/problems/1281-subtractProductAndSum.js
+++ javascript/problems/1281-subtractProductAndSum.js
@@ -6,7 +6,14 @@ var subtractProductAndSum = function(n) {
     let sum = 0;
     let product = 1;
 
-    // n will be mod until become 0
+    // we will take last digit of n by using mid 
+    // from every loop and update n with devided by 10 until n become zero
+
+    // example:
+    // n: 234, product = 1, sum = 0
+    // n: 23, product = 4, sum = 4
+    // n: 2, product = 12, sum = 7
+    // n: 0, product = 24, sum = 9
     while (n) {
         sum += n % 10;
         product *= n % 10;
