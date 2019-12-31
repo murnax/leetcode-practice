7c4bb35f57d7551678bed5b98172f6cad49d5123
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-12-31 13:42:35 +0700

    update to O(n)

--- scala/src/main/scala/SortedSquares.scala
+++ scala/src/main/scala/SortedSquares.scala
@@ -1,6 +1,22 @@
 package leetcode
 
 object SortedSquares extends App {
-  def sortedSquares(A: Array[Int]): Array[Int] =
-    A.map(n => n * n).sortWith(_ < _)
+  def sortedSquares(A: Array[Int]): Array[Int] = {
+    val result = new Array[Int](A.length)
+    var n = A.length - 1
+    var l = 0
+    var r = A.length - 1
+
+    while (l <= r) {
+      if (Math.pow(A(l), 2) > Math.pow(A(r), 2)) {
+        result(n) = Math.pow(A(l), 2).toInt
+        l += 1
+      } else {
+        result(n) = Math.pow(A(r), 2).toInt
+        r -= 1
+      }
+      n -= 1
+    }
+    result
+  }
 }
