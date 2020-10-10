575a66be6db8bd1b14a7c7d68e40ecbc36339d5a
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-10 23:33:23 +0700

    Reverse integer.

---
+++ csharp/Solution/7-ReverseInteger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class ReverseInteger
+    {
+        public static int Solution(int n)
+        {
+            int result = 0, prev = 0;
+            while (n != 0)
+            {
+                result = (result * 10) + (n % 10);
+                if ((result - (n % 10)) / 10 != prev) return 0;
+                prev = result;
+                n /= 10;
+            }
+            return result;
+        }
+    }
+}
---
+++ csharp/UnitTest/7-ReverseIntegerTest.cs
@@ -0,0 +1,19 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ReverseIntegerTest
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(0, 0)]
+        [InlineData(1534236469, 0)]
+        public void TestMethod(int n, int output)
+        {
+            Assert.Equal(output, ReverseInteger.Solution(n));
+        }
+    }
+}
