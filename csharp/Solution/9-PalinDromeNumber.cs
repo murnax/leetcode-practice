7fd39c60b2ac673a8db38aa11a3cb40327dd80ee
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-10 23:56:31 +0700

    (WIP)

---
+++ csharp/Solution/9-PalinDromeNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace Solution
+{
+    public class PalinDromeNumber
+    {
+        public static bool Solution(int x)
+        {
+            int originValue = x;
+            if (x < 0) return false;
+            if (x == 0) return true;
+
+            while (x > 0)
+            {
+
+            }
+        }
+    }
+}
---
+++ csharp/UnitTest/9-PalinDromeNumberTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Solution;
+
+namespace UnitTest
+{
+    public class PalinDromeNumberTest
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(-101, false)]
+        public void TestMethod(int x, bool output)
+        {
+            Assert.Equal(output, PalinDromeNumber.Solution(x));
+        }
+    }
+}
