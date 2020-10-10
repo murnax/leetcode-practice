e592b793147eb82c7caab4397cb325d8a06fa085
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-10 23:02:49 +0700

    Valid palindrome.

---
+++ csharp/Solution/125-ValidPalindrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Solution
+{
+    public class ValidPalindrome
+    {
+        public static bool Solution(string s)
+        {
+            char[] chars = s.ToLower().ToCharArray().Where(validCharacter).ToArray();
+            int l = 0, r = chars.Length - 1;
+            while (l < r) if (chars[l++] != chars[r--]) return false;
+            return true;
+        }
+
+        private static bool validCharacter(char c)
+        {
+            return new Regex(@"^[a-zA-Z0-9]*$").IsMatch(c.ToString());
+        }
+    }
+}
---
+++ csharp/UnitTest/125-ValidPalindromeTest.cs
@@ -0,0 +1,16 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ValidPalindromeTest
+    {
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        public void TestMethod(string s, bool output)
+        {
+            Assert.Equal(output, ValidPalindrome.Solution(s));
+        }
+    }
+}
