592252272ebab319b5d8fbc53d9bb8a1381a1c69
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-10 21:31:10 +0700

    Another problem.

---
+++ csharp/Solution/5-LongestPalindromicSubstring.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class LongestPalindromicSubstring
+    {
+        public static string Solution(string s)
+        {
+            var n = s.Length;
+            for (var i = n; i > 0; i--)
+            {
+                for (var j = 0; j <= n - i; j++)
+                {
+                    var substr = s.Substring(j, i);
+                    if (isPalindrome(substr)) return substr;
+                }
+            }
+            return "";
+        }
+
+        private static bool isPalindrome(string s)
+        {
+            int l = 0, r = s.Length - 1;
+            while (l < r)
+            {
+                if (s[l++] != s[r--]) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ csharp/UnitTest/5-LongestPalindromicSubstringTest.cs
@@ -0,0 +1,15 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class LongestPalindromicSubstringTest
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        public void TestMethod(string s, string output)
+        {
+            Assert.Equal(output, LongestPalindromicSubstring.Solution(s));
+        }
+    }
+}
