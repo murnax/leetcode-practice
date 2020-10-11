d691836163cfd803faf87b4cb99bb6db9cf25acd
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 12:53:02 +0700

    ValidParentheses.

---
+++ csharp/Solution/20-ValidParentheses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class ValidParentheses
+    {
+        public static bool Solution(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach(var c in s)
+            {
+                if (c == '(') stack.Push(')');
+                else if (c == '{') stack.Push('}');
+                else if (c == '[') stack.Push(']');
+                else if (stack.Count == 0 || c != stack.Pop()) return false;
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ csharp/UnitTest/20-ValidParenthesesTest.cs
@@ -0,0 +1,22 @@
+﻿using Solution;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+    public class ValidParenthesesTest
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("([)]", false)]
+        [InlineData("{[]}", true)]
+        public void TestMethod(string s, bool output)
+        {
+            Assert.Equal(output, ValidParentheses.Solution(s));
+        }
+    }
+}
