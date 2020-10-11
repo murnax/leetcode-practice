c8b63a794c8e81df3143af5ee29aa48e7578e632
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 11:24:57 +0700

    Three sum.

---
+++ csharp/Solution/15-ThreeSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class ThreeSum
+    {
+        public static IList<IList<int>> Solution(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int l = i + 1;
+                int r = nums.Length - 1;
+                while (l < r)
+                {
+                    if (nums[i] == 0 - nums[l] - nums[r])
+                    {
+                        result.Add(new List<int> { nums[i], nums[l], nums[r] });
+
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+
+                        l++;
+                        r--;
+                    }
+                    else if (nums[i] < 0 - nums[l] - nums[r])
+                    {
+                        l++;
+                    }
+                    else
+                    {
+                        r--;
+                    }
+                }
+                while (i < nums.Length - 1 && nums[i] == nums[i + 1]) i++;
+            }
+            return result;
+        }
+    }
+}
---
+++ csharp/UnitTest/15-ThreeSumTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ThreeSumTest
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestMethod(int[] nums, List<List<int>> output)
+        {
+            new List<List<int>>();
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new List<object[]>()
+                {
+                    new object[]
+                    {
+                        new int[]{ -1, 0, 1, 2, -1, -4 },
+                        new List<List<int>>
+                        {
+                            new List<int> { -1, -1, -2 },
+                            new List<int> { -1, 0, 1 }
+                        }
+                    },
+                    new object[]
+                    {
+                        new int[]{},
+                        new List<List<int>>{}
+                    },
+                    new object[]
+                    {
+                        new int[]{ 0 },
+                        new List<List<int>>{}
+                    }
+                };
+            }
+        }
+    }
+}
