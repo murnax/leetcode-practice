9f2b02298d73f23be2a262143265f234359e3e44
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 10:39:50 +0700

    Longest common prefix.

---
+++ csharp/Solution/14-LongestCommonPrefix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+    public class LongestCommonPrefix
+    {
+        public static string Solution(string[] strs)
+        {
+            return strs.Length > 0 ? strs.Aggregate((prev, next) =>
+            {
+                var i = 0;
+                while (i < prev.Length && i < next.Length && prev[i] == next[i]) i++;
+                return prev.Substring(0, i);
+            }).ToString() : string.Empty;
+        }
+    }
+}
---
+++ csharp/UnitTest/14-LongestCommonPrefixTest.cs
@@ -0,0 +1,18 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class LongestCommonPrefixTest
+    {
+        [Theory]
+        [InlineData(new string[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new string[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new string[] { "ab", "a" }, "a")]
+        [InlineData(new string[] { }, "")]
+        public void TestMethod(string[] strs, string output)
+        {
+            Assert.Equal(output, LongestCommonPrefix.Solution(strs));
+        }
+    }
+}
