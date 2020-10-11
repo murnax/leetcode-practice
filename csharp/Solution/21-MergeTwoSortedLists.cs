ce289b9890bc32eb5604b5d55e76855016ceebe2
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 13:04:30 +0700

    Merge two sorted lists.

---
+++ csharp/Solution/21-MergeTwoSortedLists.cs
@@ -0,0 +1,29 @@
+﻿using Solution.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class MergeTwoSortedLists
+    {
+        public static ListNode<int> Solution(ListNode<int> l1, ListNode<int> l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            ListNode<int> result;
+            if (l1.Val < l2.Val)
+            {
+                result = l1;
+                result.Next = Solution(l1.Next, l2);
+            }
+            else
+            {
+                result = l2;
+                result.Next = Solution(l1, l2.Next);
+            }
+            return result;
+        }
+    }
+}
---
+++ csharp/UnitTest/21-MergeTwoSortedListsTest.cs
@@ -0,0 +1,45 @@
+﻿using Solution;
+using Solution.Utils;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTest
+{
+    public class MergeTwoSortedListsTest
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestMethod(ListNode<int> l1, ListNode<int> l2, List<int> output)
+        {
+            Assert.Equal(output, MergeTwoSortedLists.Solution(l1, l2).ToList());
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[]
+                    {
+                        new List<int>{1,2,4 }.ToListNode(),
+                        new List<int>{1,3,4 }.ToListNode(),
+                        new List<int>{1,1,2,3,4,4 }
+                    },
+                    new object[]
+                    {
+                        new List<int>{ }.ToListNode(),
+                        new List<int>{ }.ToListNode(),
+                        new List<int>{ }
+                    },
+                    new object[]
+                    {
+                        new List<int>{ }.ToListNode(),
+                        new List<int>{ 0 }.ToListNode(),
+                        new List<int>{ 0 }
+                    }
+                };
+            }
+        }
+    }
+}
