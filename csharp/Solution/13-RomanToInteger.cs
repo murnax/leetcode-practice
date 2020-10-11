4b1669ca191ae313d0730dbf5a8714526b8836d5
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 10:11:00 +0700

    Roman to integer.

---
+++ csharp/Solution/13-RomanToInteger.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Solution
+{
+    public class RomanToInteger
+    {
+        public static int Solution(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                char next = i < s.Length - 1 ? s[i + 1] : '0';
+
+                if (c == 'I')
+                {
+                    if (next == 'V')
+                    {
+                        result += 4;
+                        i++;
+                    }
+                    else if (next == 'X')
+                    {
+                        result += 9;
+                        i++;
+                    } else
+                    {
+                        result += 1;
+                    }
+                }
+
+                if (c == 'V')
+                {
+                    result += 5;
+                }
+
+                if (c == 'X')
+                {
+                    if (next == 'L')
+                    {
+                        result += 40;
+                        i++;
+                    }
+                    else if (next == 'C')
+                    {
+                        result += 90;
+                        i++;
+                    }
+                    else
+                    {
+                        result += 10;
+                    }
+                }
+
+                if (c == 'L')
+                {
+                    result += 50;
+                }
+
+                if (c == 'C')
+                {
+                    if (next == 'D')
+                    {
+                        result += 400;
+                        i++;
+                    }
+                    else if(next == 'M')
+                    {
+                        result += 900;
+                        i++;
+                    }
+                    else
+                    {
+                        result += 100;
+                    }
+                }
+
+                if (c == 'D')
+                {
+                    result += 500;
+                }
+
+                if (c == 'M')
+                {
+                    result += 1000;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ csharp/UnitTest/13-RomanToIntegerTest.cs
@@ -0,0 +1,19 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class RomanToIntegerTest
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void TestMethod(string s, int output)
+        {
+            Assert.Equal(output, RomanToInteger.Solution(s));
+        }
+    }
+}
