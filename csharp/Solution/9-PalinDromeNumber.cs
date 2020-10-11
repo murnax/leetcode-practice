d738bc1f5aed60770cce1b4a9b6697bcf04049c0
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 09:24:26 +0700

    Palin drome number.

--- csharp/Solution/9-PalinDromeNumber.cs
+++ csharp/Solution/9-PalinDromeNumber.cs
@@ -8,10 +8,13 @@
             if (x < 0) return false;
             if (x == 0) return true;
 
+            int reversedValue = 0;
             while (x > 0)
             {
-
+                reversedValue = (reversedValue * 10) + (x % 10);
+                x /= 10;
             }
+            return originValue == reversedValue;
         }
     }
 }
