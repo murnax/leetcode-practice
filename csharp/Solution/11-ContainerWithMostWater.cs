2d48eec3acf18ee6c9f0a44321420a9b258d815e
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-11 09:47:10 +0700

    Container with most water.

---
+++ csharp/Solution/11-ContainerWithMostWater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solution
+{
+    public class ContainerWithMostWater
+    {
+        public static int Solution(int[] height)
+        {
+            int result = 0, i = 0, j = height.Length - 1;
+            while (i < j)
+            {
+                int area = Math.Min(height[i], height[j]) * (j - i);
+                result = Math.Max(result, area);
+                if (height[i] < height[j]) i++;
+                else j--;
+            }
+            return result;
+        }
+    }
+}
---
+++ csharp/UnitTest/11-ContainerWithMostWaterTest.cs
@@ -0,0 +1,15 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ContainerWithMostWaterTest
+    { 
+        [Theory]
+        [InlineData(new int[] { 1,8,6,2,5,4,8,3,7}, 49)]
+        public void TestMethod(int[] height, int output)
+        {
+            Assert.Equal(output, ContainerWithMostWater.Solution(height));  
+        }
+    }
+}
