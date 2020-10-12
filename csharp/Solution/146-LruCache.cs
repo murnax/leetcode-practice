a1f50f2f852298359b7434d3d43e681baf679c3f
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 00:54:09 +0700

    LRU cache.

---
+++ csharp/Solution/146-LruCache.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    // https://leetcode.com/problems/lru-cache/discuss/126734/C-LinkedList-%2B-Dictionary
+    public class LruCache
+    {
+        private readonly int _capacity;
+        private int _count;
+        private readonly LinkedList<CacheItem> _items;
+        private readonly Dictionary<int, LinkedListNode<CacheItem>> _keys;
+        
+        public LruCache(int capacity)
+        {
+            _count = 0;
+            _capacity = capacity;
+            _items = new LinkedList<CacheItem>();
+            _keys = new Dictionary<int, LinkedListNode<CacheItem>>(capacity);
+        }
+
+        public int Get(int key)
+        {
+            if (!_keys.ContainsKey(key)) return -1;
+
+            var cacheItem = _keys[key];
+            if (cacheItem != _items.First)
+            {
+                _items.Remove(cacheItem);
+                _items.AddFirst(cacheItem);
+            }            
+            return cacheItem.Value.Value;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (!_keys.ContainsKey(key))
+            {
+                _keys[key] = _items.AddFirst(new CacheItem(key, value));
+
+                if (_count == _capacity)
+                {
+                    var last = _items.Last;
+                    _keys.Remove(last.Value.Key);
+                    _items.RemoveLast();
+                }
+                else
+                {
+                    _count++;
+                }
+            }
+            else
+            {
+                var cacheItem = _keys[key];
+                cacheItem.Value.Value = value;
+
+                if (cacheItem != _items.First)
+                {
+                    _items.Remove(cacheItem);
+                    _items.AddFirst(cacheItem);
+                }
+            }
+        }
+
+        private class CacheItem
+        {
+            public CacheItem(int key, int value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public int Key { get; }
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ csharp/UnitTest/146-LruCacheTest.cs
@@ -0,0 +1,40 @@
+﻿using Solution;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest
+{
+    public class LruCacheTest
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestMethod(LruCache lruCache)
+        {
+            lruCache.Put(1, 1);
+            lruCache.Put(2, 2);
+            Assert.Equal(1, lruCache.Get(1));
+            lruCache.Put(3, 3);
+            Assert.Equal(-1, lruCache.Get(2));
+            lruCache.Put(4, 4);
+            Assert.Equal(-1, lruCache.Get(1));
+            Assert.Equal(3, lruCache.Get(3));
+            Assert.Equal(4, lruCache.Get(4));
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new List<object[]>()
+                {
+                    new object[]
+                    {
+                        new LruCache(2),
+                    }
+                };
+            }
+        }
+    }
+}
