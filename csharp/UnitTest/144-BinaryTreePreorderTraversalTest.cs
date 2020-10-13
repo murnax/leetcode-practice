9f64457717c4b4557908a95ee3535a414447019f
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 16:43:37 +0700

    Replace manual TreeNode constructing with .ToTreeNode.

--- csharp/UnitTest/144-BinaryTreePreorderTraversalTest.cs
+++ csharp/UnitTest/144-BinaryTreePreorderTraversalTest.cs
@@ -27,21 +27,9 @@ namespace UnitTest
             {
                 var driverData = new List<object[]>();
 
-                TreeNode<int> root2 = new TreeNode<int>(1);
-                root2.Left = new TreeNode<int>(2);
-                root2.Right = new TreeNode<int>(3);
-                driverData.Add(new object[] { root2, new List<int> { 1, 2, 3 } });
-
-                TreeNode<int> root = new TreeNode<int>(1);
-                root.Right = new TreeNode<int>(2);
-                root.Right.Left = new TreeNode<int>(3);
-                root.Right.Right = new TreeNode<int>(4);
-                root.Right.Right.Left = new TreeNode<int>(5);
-                driverData.Add(new object[]{root, new List<int>{ 1, 2, 3, 4, 5 } });
-
-                TreeNode<int> root3 = new TreeNode<int>(1);
-                root3.Right = new TreeNode<int>(2);
-                driverData.Add(new object[] { root3, new List<int> { 1, 2 } });
+                driverData.Add(new object[] { new List<int?> { 1, null, 2, null, null, 3 }.ToTreeNode(), new List<int> { 1, 2, 3 } });
+                driverData.Add(new object[] { new List<int?> { 1, 2, 3, 4, 5 }.ToTreeNode(), new List<int> { 1, 2, 4, 5, 3 } });
+                driverData.Add(new object[] { new List<int?> { 1, 2, 3 }.ToTreeNode(), new List<int> { 1, 2, 3 } });
 
                 return driverData;
             }
