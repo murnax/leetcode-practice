b36875b534df73315d641831ad8b26e7dc0b993c
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 12:34:37 +0700

    Add another implementation of min stack using one stack using Value Tuple.

--- csharp/Solution/155-MinStack.cs
+++ csharp/Solution/155-MinStack.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Solution
 {
-    public class MinStack
+    public interface IMinStack
+    {
+        public void Push(int x);
+        public void Pop();
+        public int Top();
+        public int GetMin();
+    }
+
+    public class MinStackWithTwoLinkedLists : IMinStack
     {
         private readonly LinkedList<int> _vals;
         private readonly LinkedList<int> _minVals;
 
-        public MinStack()
+        public MinStackWithTwoLinkedLists()
         {
             _vals = new LinkedList<int>();
             _minVals = new LinkedList<int>();
@@ -33,6 +42,43 @@ namespace Solution
         public int GetMin()
         {
             return _minVals.Last.Value;
+        }
+    }
+
+    public class MinStackWithOneStack : IMinStack
+    {
+        private readonly Stack<(int Value, int Min)> _stack;
+
+        public MinStackWithOneStack()
+        {
+            _stack = new Stack<(int, int)>();
+        }
+
+        public int GetMin()
+        {
+            return _stack.Peek().Min;
+        }
+
+        public void Pop()
+        {
+            _stack.Pop();
+        }
+
+        public void Push(int x)
+        {
+            if (_stack.Count == 0)
+            {
+                _stack.Push((x, x));
+            }
+            else
+            {
+                _stack.Push((x, Math.Min(x, _stack.Peek().Min)));
+            }
+        }
+
+        public int Top()
+        {
+            return _stack.Peek().Value;
         }
     }
 }
--- csharp/UnitTest/155-MinStackTest.cs
+++ csharp/UnitTest/155-MinStackTest.cs
@@ -1,14 +1,15 @@
 ﻿using Solution;
+using System.Collections.Generic;
 using Xunit;
 
 namespace UnitTest
 {
     public class MinStackTest
     {
-        [Fact]
-        public void TestMethod()
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestMethod(IMinStack stack)
         {
-            MinStack stack = new MinStack();
             stack.Push(-2);
             stack.Push(0);
             stack.Push(-3);
@@ -16,6 +17,18 @@ namespace UnitTest
             stack.Pop();
             Assert.Equal(0, stack.Top());
             Assert.Equal(-2, stack.GetMin());
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[]{ new MinStackWithOneStack() },
+                    new object[]{ new MinStackWithTwoLinkedLists() },
+                };
+            }
         }
     }
 }
