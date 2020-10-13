0157d971f967c37b8b20a11d78642fc22fb12d90
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 13:25:12 +0700

    Add TreeNode class as utility class.

---
+++ csharp/Solution/Utils/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution.Utils
+{
+    class TreeNode<T>
+    {        
+        public T Val { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+
+        public TreeNode(T val) { Val = val; }
+    }
+}
