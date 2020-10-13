19f88870ba6a4a2c0b7c1d84f18093c37b15b5b9
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 18:33:25 +0700

    Replace all tree nodes manual creation with helper.

--- javascript/problems/144-binary-tree-preorder-traversal.spec.js
+++ javascript/problems/144-binary-tree-preorder-traversal.spec.js
@@ -1,32 +1,18 @@
 const chai = require('chai');
 const expect = chai.expect;
 const itParam = require('mocha-param');
+const { convertArrayToBinaryTree } = require('../helper/treeNode');
 
 const preorderTraversal = require('./144-binary-tree-preorder-traversal');
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
 
 describe('in order traversal', () => {
-
-    const input1 = new TreeNode(1);
-    input1.right = new TreeNode(2);
-    input1.right.left = new TreeNode(3);
-
-    const input2 = new TreeNode(1);
-    input2.left = new TreeNode(2);
-    input2.right = new TreeNode(3);
-    input2.left.left = new TreeNode(4);
-    input2.right.right = new TreeNode(5);
-
     const testCases = [
         {
-            root: input1,
+            root: convertArrayToBinaryTree([1, null, 2, null, null, 3]),
             result: [1, 2, 3]
         },
         {
-            root: input2,
+            root: convertArrayToBinaryTree([1, 2, 3, 4, null, null, 5]),
             result: [1,2,4,3,5]
         }
     ];
--- javascript/problems/98-isValidBST.spec.js
+++ javascript/problems/98-isValidBST.spec.js
@@ -1,41 +1,22 @@
 const chai = require('chai');
 const expect = chai.expect;
 const itParam = require('mocha-param');
+const { convertArrayToBinaryTree } = require('../helper/treeNode');
 
 const isValidBST = require('./98-isValidBST');
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
 
 describe('isValidBST()', () => {
-    const input1 = new TreeNode(2);
-    input1.left = new TreeNode(1);
-    input1.right = new TreeNode(3);
-
-    const input2 = new TreeNode(5);
-    input2.left = new TreeNode(1);
-    input2.right = new TreeNode(4);
-    input2.right.left = new TreeNode(3);
-    input2.right.right = new TreeNode(6);
-
-    const input3 = new TreeNode(10);
-    input3.left = new TreeNode(5);
-    input3.right = new TreeNode(15);
-    input3.right.left = new TreeNode(6);
-    input3.right.right = new TreeNode(20);
-
     const testCases = [
         {
-            root: input1,
+            root: convertArrayToBinaryTree([2, 1, 3]),
             result: true
         },
         {
-            root: input2,
+            root: convertArrayToBinaryTree([5, 1, 4, null, null, 3, 6]),
             result: false
         },
         {
-            root: input3,
+            root: convertArrayToBinaryTree([10, 5, 15, 6, 20]),
             result: false
         }
     ];
