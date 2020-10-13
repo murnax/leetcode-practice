27b08bb0adbd0f133f090fe1dcb323b8c2be9b77
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 12:16:06 +0700

    [C#] Min stack.

---
+++ csharp/Solution/155-MinStack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Solution
+{
+    public class MinStack
+    {
+        private readonly LinkedList<int> _vals;
+        private readonly LinkedList<int> _minVals;
+
+        public MinStack()
+        {
+            _vals = new LinkedList<int>();
+            _minVals = new LinkedList<int>();
+        }
+
+        public void Push(int x)
+        {
+            if (_minVals.Count == 0 || x <= _minVals.Last.Value) _minVals.AddLast(x);
+            _vals.AddLast(x);
+        }
+
+        public void Pop()
+        {
+            if (Top() == GetMin()) _minVals.RemoveLast();
+            _vals.RemoveLast();
+        }
+
+        public int Top()
+        {
+            return _vals.Last.Value;
+        }
+
+        public int GetMin()
+        {
+            return _minVals.Last.Value;
+        }
+    }
+}
---
+++ csharp/UnitTest/155-MinStackTest.cs
@@ -0,0 +1,21 @@
+﻿using Solution;
+using Xunit;
+
+namespace UnitTest
+{
+    public class MinStackTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            MinStack stack = new MinStack();
+            stack.Push(-2);
+            stack.Push(0);
+            stack.Push(-3);
+            Assert.Equal(-3, stack.GetMin());
+            stack.Pop();
+            Assert.Equal(0, stack.Top());
+            Assert.Equal(-2, stack.GetMin());
+        }
+    }
+}
