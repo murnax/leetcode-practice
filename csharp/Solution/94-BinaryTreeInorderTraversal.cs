c42459a94619714b5c92148895028f26ff5d4221
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 16:55:15 +0700

    [C#] binary tree inorder traversal (recursion).

---
+++ csharp/Solution/94-BinaryTreeInorderTraversal.cs
@@ -0,0 +1,25 @@
+﻿using Solution.Utils;
+using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public class BinaryTreeInorderTraversalRecursive
+    {
+        public static List<int> Solution(TreeNode<int> root)
+        {
+            return Inorder(root, new List<int>());
+        }
+
+        private static List<int> Inorder(TreeNode<int> root, List<int> nodes)
+        {
+            if (root == null) return nodes;
+
+            if (root.Left != null) Inorder(root.Left, nodes);
+            nodes.Add(root.Val);
+            if (root.Right != null) Inorder(root.Right, nodes);
+
+            return nodes;
+        }
+    }
+}
---
+++ csharp/UnitTest/94-BinaryTreeInorderTraversalTest.cs
@@ -0,0 +1,49 @@
+﻿using Solution;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Solution.Utils;
+
+namespace UnitTest
+{
+    public class _BinaryTreeInorderTraversalTest
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestMethod_Recursion(TreeNode<int> root, List<int> output)
+        {
+            Assert.Equal(output, BinaryTreeInorderTraversalRecursive.Solution(root));
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[]
+                    {
+                        new List<int?>{1, null, 2, null, null, 3 }.ToTreeNode(), 
+                        new List<int> { 1, 3, 2 },
+                    },
+                    new object[]
+                    {
+                        new List<int?>{ }.ToTreeNode(),
+                        new List<int> { },
+                    },
+                    new object[]
+                    {
+                        new List<int?>{1, 2 }.ToTreeNode(),
+                        new List<int>{ 2, 1 }
+                    },
+                    new object[]
+                    {
+                        new List<int?>{1, null, 2 }.ToTreeNode(),
+                        new List<int>{1, 2 }
+                    }
+                };
+            }
+        }
+    }
+}
