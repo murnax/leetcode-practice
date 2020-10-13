deee608a9a7f6c381a122aae04ede03fc7082142
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2020-10-13 18:18:32 +0700

    Replace TreeNode manual creation with helper function.

---
+++ javascript/helper/treeNode.js
@@ -0,0 +1,21 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right= null;
+};
+
+
+
+function convertArrayToBinaryTree(array, index = 1) {
+    if (index > array.length) return null;
+
+    const value = array[index - 1];
+    if (!value) return null;
+
+    const node = new TreeNode(value);
+    node.left = convertArrayToBinaryTree(array, index * 2);
+    node.right = convertArrayToBinaryTree(array, index * 2 + 1);
+    return node;
+}
+
+exports.TreeNode = TreeNode;
+exports.convertArrayToBinaryTree = convertArrayToBinaryTree;
--- javascript/problems/94-binary-tree-inorder-traversal.spec.js
+++ javascript/problems/94-binary-tree-inorder-traversal.spec.js
@@ -1,27 +1,31 @@
 const chai = require('chai');
 const expect = chai.expect;
 const itParam = require('mocha-param');
+const { convertArrayToBinaryTree } = require('../helper/treeNode');
 
 const inorderTraversal = require('./94-binary-tree-inorder-traversal');
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
 
 describe('in order traversal', () => {
-
-    const input1 = new TreeNode(1);
-    input1.right = new TreeNode(2);
-    input1.right.left = new TreeNode(3);
-
     const testCases = [
         {
-            root: input1,
+            root: convertArrayToBinaryTree([1, null, 2, null, null, 3]),
             result: [1, 3, 2]
+        },
+        {
+            root: convertArrayToBinaryTree([]),
+            result: []
+        },
+        {
+            root: convertArrayToBinaryTree([1, 2]),
+            result: [2, 1]
+        },
+        {
+            root: convertArrayToBinaryTree([1, null, 2]),
+            result: [1, 2]
         }
     ];
 
-    itParam("root = ${JSON.stringify(value.root, null, 4} should return ${value.result}", testCases, testCase => {
-        expect(inorderTraversal(testCase.root)).to.equal(testCase.result);
-    })
+    itParam("root = ${JSON.stringify(value.root, null, 4)} should return ${JSON.stringify(value.result, null)}", testCases, testCase => {
+        expect(inorderTraversal(testCase.root)).to.deep.equal(testCase.result);
+    });
 });
