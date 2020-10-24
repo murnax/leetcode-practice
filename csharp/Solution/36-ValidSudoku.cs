18d0511c9f62ea7f3a1e84cc9c07f8b271a533a5
Suratin Pattanawongthai <suratin.elec@gmail.com>
2020-10-24 22:34:52 +0700

    (WIP) [36] C#

---
+++ csharp/Solution/36-ValidSudoku.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public class ValidSudoku
+    {
+        public static bool Solution(char[][] board)
+        {
+            var rows = new Dictionary<string, bool>();
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    if (board[i][j] != '.')
+                    {
+                        
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
