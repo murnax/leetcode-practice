528792f6c756768d3486fab18dee17f225376e0c
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2021-02-12 00:08:24 +0700

    Rename file.

--- scala/src/test/scala/DefangingAnIPAddressTest.scala
+++ scala/src/test/scala/DefangingAnIPAddressTest.scala
@@ -1,7 +1,7 @@
 import leetcode.DefangingAnIPAddress
 import org.scalatest.FunSuite
 
-class DefangingAnIPAddress extends FunSuite {
+class DefangingAnIPAddressTest extends FunSuite {
   test("DefangingAnIPAddress") {
     assert(DefangingAnIPAddress.defangIPaddr("1.1.1.1") === "1[.]1[.]1[.]1")
     assert(DefangingAnIPAddress.defangIPaddr("255.100.50.0") === "255[.]100[.]50[.]0")
