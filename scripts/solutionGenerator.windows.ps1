906f780edc74bc0a95d483949bc2a54beebfec87
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2021-08-05 22:50:20 +0700

    Move file to target folders

--- scripts/solutionGenerator.windows.ps1
+++ scripts/solutionGenerator.windows.ps1
@@ -10,15 +10,21 @@ $problemName = (Get-Culture).TextInfo.ToTitleCase($name) -replace '[^a-zA-Z0-9]'
 Copy-Item ".\javascript.solution.template" -Destination ".\$problemName.js"
 Copy-Item ".\javascript.test.template" -Destination ".\$problemName.spec.js"
 ((Get-Content -path ".\$problemName.spec.js" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$problemName.spec.js"
+Move-Item -Path ".\$problemName.js" -Destination "..\javascript\src"
+Move-Item -Path ".\$problemName.spec.js" -Destination "..\javascript\src"
 
 # Scala
 Copy-Item ".\scala.solution.template" -Destination ".\$problemName.scala"
 ((Get-Content -path ".\$problemName.scala" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$problemName.scala"
 Copy-Item ".\scala.test.template" -Destination ".\$($problemName)Test.scala"
 ((Get-Content -path ".\$($problemName)Test.scala" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$($problemName)Test.scala"
+Move-Item -Path ".\$problemName.scala" -Destination "..\scala\src\main\scala"
+Move-Item -Path ".\$($problemName)Test.scala" -Destination "..\scala\src\test\scala"
 
 # C#
 Copy-Item ".\csharp.solution.template" -Destination ".\$problemName.cs"
 ((Get-Content -path ".\$problemName.cs" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$problemName.cs"
 Copy-Item ".\csharp.test.template" -Destination ".\$($problemName)Test.cs"
 ((Get-Content -path ".\$($problemName)Test.cs" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$($problemName)Test.cs"
+Move-Item -Path ".\$problemName.cs" -Destination "..\csharp\Solution"
+Move-Item -Path ".\$($problemName)Test.cs" -Destination "..\csharp\UnitTest"
