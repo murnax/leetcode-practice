f6bd6ef12f0c1fbd7a0521b23441152b55193d10
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2021-08-05 22:15:42 +0700

    Add logic to generate C# code.

--- scripts/solutionGenerator.windows.ps1
+++ scripts/solutionGenerator.windows.ps1
@@ -16,3 +16,9 @@ Copy-Item ".\scala.solution.template" -Destination ".\$problemName.scala"
 ((Get-Content -path ".\$problemName.scala" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$problemName.scala"
 Copy-Item ".\scala.test.template" -Destination ".\$($problemName)Test.scala"
 ((Get-Content -path ".\$($problemName)Test.scala" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$($problemName)Test.scala"
+
+# C#
+Copy-Item ".\csharp.solution.template" -Destination ".\$problemName.cs"
+((Get-Content -path ".\$problemName.cs" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$problemName.cs"
+Copy-Item ".\csharp.test.template" -Destination ".\$($problemName)Test.cs"
+((Get-Content -path ".\$($problemName)Test.cs" -Raw) -replace '%problemName%', $problemName) | Set-Content -Path ".\$($problemName)Test.cs"
