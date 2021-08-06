67e73568e579c1e46438f7246acb80f2abb438e5
Suratin Pattanawongthai <suratin.elec@gmail.com>
2021-08-07 00:24:32 +0700

    clean up code

--- python/src/add_two_number.py
+++ python/src/add_two_number.py
@@ -5,7 +5,7 @@ class AddTwoNumber:
         carry = 0
         result = None
         currentNode = None
-        while (l1 is not None or l2 is not None or carry):
+        while l1 or l2 or carry:
             sum = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
             if sum >= 10:
                 carry = 1
@@ -13,7 +13,7 @@ class AddTwoNumber:
             else:
                 carry = 0
             node = ListNode(sum)
-            if result is None:
+            if not result:
                 result = currentNode = node
             else:
                 currentNode.next = node
@@ -22,7 +22,3 @@ class AddTwoNumber:
             l2 = (l2.next if l2 else None)
 
         return result
-
-l1 = ListNode(1, ListNode(2, ListNode(3)))
-l2 = ListNode(5)
-result = AddTwoNumber().solution(l1, l2)
