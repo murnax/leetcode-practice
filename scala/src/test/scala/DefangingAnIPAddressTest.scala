4733fb1ea02899b81b8de32bba5d4ea202a7da92
Suratin Pattanawongthai <Suratin.Pattanawongthai@agoda.com>
2021-08-17 22:36:10 +0700

    rewrite test with Table

--- scala/src/test/scala/DefangingAnIPAddressTest.scala
+++ scala/src/test/scala/DefangingAnIPAddressTest.scala
@@ -1,9 +1,17 @@
 import leetcode.DefangingAnIPAddress
+import org.scalatest.prop.TableDrivenPropertyChecks._
 import org.scalatest.FunSuite
 
 class DefangingAnIPAddressTest extends FunSuite {
-  test("DefangingAnIPAddress") {
-    assert(DefangingAnIPAddress.defangIPaddr("1.1.1.1") === "1[.]1[.]1[.]1")
-    assert(DefangingAnIPAddress.defangIPaddr("255.100.50.0") === "255[.]100[.]50[.]0")
+  val testCases = Table(
+    ("address", "expected"),
+    ("1.1.1.1", "1[.]1[.]1[.]1"),
+    ("255.100.50.0", "255[.]100[.]50[.]0")
+  )
+
+  testCases.foreach { i =>
+    test(s"f(${i._1}) should equal to ${i._2}") {
+      assert(DefangingAnIPAddress.defangIPaddr(i._1) === i._2)
+    }
   }
 }
